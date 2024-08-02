103accd853890cc640e5659d7cc34b3684c7c924
Ahmad01Almasri <ahmad.2271222@gmail.com>
2024-08-02 16:52:35 +0300

    ai  chat page ui and logic && services apps page

--- lib/features/ai chat/presentation/pages/chat.dart
+++ lib/features/ai chat/presentation/pages/chat.dart
@@ -5,6 +5,9 @@ import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+import 'package:tourist_guide/core/utils/app_colors.dart';
+import 'package:tourist_guide/core/utils/app_text_styles.dart';
 
 class GeminiPage extends StatefulWidget {
   const GeminiPage({super.key});
@@ -22,9 +25,22 @@ class _GeminiPageState extends State<GeminiPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.lightGrey,
       appBar: AppBar(
-        title: const Text('How can i help you'),
-        backgroundColor: Colors.blue,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(
+            size: 30,
+            Icons.arrow_back,
+            color: AppColors.white,
+          ),
+        ),
+        title: Text(
+          'SYRAVEL Bot , can I help you?',
+          style: AppTextStyles.poppins400style20
+              .copyWith(fontWeight: FontWeight.bold, color: AppColors.white),
+        ),
+        backgroundColor: AppColors.primaryColor,
       ),
       body: _buildUI(),
     );
@@ -32,16 +48,78 @@ class _GeminiPageState extends State<GeminiPage> {
 
   Widget _buildUI() {
     return DashChat(
-        inputOptions: InputOptions(
-            trailing: [
+      messageListOptions: MessageListOptions(
+        dateSeparatorBuilder: (date) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Center(
+            child: Text(
+              date.toLocal().toString().split(' ')[0],
+              style: AppTextStyles.poppinsBoldstyle24
+                  .copyWith(color: AppColors.white),
+            ),
+          ),
+        ),
+      ),
+      messageOptions: MessageOptions(
+        showTime: false, // إخفاء الوقت الافتراضي
+        messageTextBuilder:
+            (ChatMessage msg, ChatMessage? previousMsg, ChatMessage? nextMsg) {
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  msg.text,
+                  style: TextStyle(
+                    color: msg.user.id == currentUser.id
+                        ? AppColors.white
+                        : AppColors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(DateFormat('HH :mm').format(msg.createdAt),
+                    style: AppTextStyles.poppinsBoldstyle16
+                        .copyWith(color: AppColors.black38, fontSize: 12)),
+              ],
+            ),
+          );
+        },
+        messageDecorationBuilder:
+            (ChatMessage msg, ChatMessage? previousMsg, ChatMessage? nextMsg) {
+          bool isUser = msg.user.id == currentUser.id;
+          return BoxDecoration(
+            color: isUser ? AppColors.primaryColor : AppColors.black38,
+            borderRadius: BorderRadius.circular(10.0),
+          );
+        },
+      ),
+      inputOptions: InputOptions(
+        inputTextStyle:
+            AppTextStyles.poppinsBoldstyle18.copyWith(color: AppColors.black),
+        inputDecoration: InputDecoration(
+          hintText: 'Type a message...',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: AppColors.offWhite,
+          contentPadding: EdgeInsets.all(10.0),
+        ),
+        trailing: [
           IconButton(
-            icon: const Icon(Icons.image),
+            icon: Icon(Icons.image, color: AppColors.black),
             onPressed: _sendMediaMessage,
-          )
-        ]),
-        currentUser: currentUser,
-        onSend: _sendMessage,
-        messages: messages);
+          ),
+        ],
+      ),
+      currentUser: currentUser,
+      onSend: _sendMessage,
+      messages: messages,
+    );
   }
 
   void _sendMessage(ChatMessage chatMessage) {
@@ -84,12 +162,13 @@ class _GeminiPageState extends State<GeminiPage> {
 
   void _sendMediaMessage() async {
     ImagePicker picker = ImagePicker();
+
     XFile? file = await picker.pickImage(source: ImageSource.gallery);
     if (file != null) {
       ChatMessage chatMessage = ChatMessage(
           user: currentUser,
           createdAt: DateTime.now(),
-          text:'Do you want to know details about this thing?',
+          text: 'Do you want to know details about this thing?',
           // 'tell me what kind the chemical analyze is this and is this chemical analyze is sick or not and explain why ,translate the answer to arabic',
 
           medias: [
--- lib/features/services/presentation/pages/services_page.dart
+++ lib/features/services/presentation/pages/services_page.dart
@@ -1,1 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tourist_guide/core/utils/app_colors.dart';
+import 'package:tourist_guide/core/utils/app_constants.dart';
+import 'package:tourist_guide/core/utils/app_text_styles.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
+class ServicesScreen extends StatefulWidget {
+  ServicesScreen({super.key});
+
+  @override
+  State<ServicesScreen> createState() => _ServicesScreenState();
+}
+
+class _ServicesScreenState extends State<ServicesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon:
+                Icon(size: 35, Icons.arrow_back, color: AppColors.primaryColor),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Text(
+            'Services',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.poppinsBoldstyle24
+                .copyWith(color: AppColors.primaryColor),
+          ),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: AppConstants.appServices.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Card(
+                  color: AppColors.offWhite.withOpacity(0.2),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Image.asset(
+                                  AppConstants.appServices[index]['image']!,
+                                  width: 120,
+                                  height: 120),
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    AppConstants.appServices[index]['title']!,
+                                    style: AppTextStyles.poppinsBoldstyle24,
+                                  ),
+                                  Text(
+                                    AppConstants.appServices[index]
+                                        ['description']!,
+                                    style: AppTextStyles.poppinsThinW6000style22
+                                        .copyWith(
+                                            fontSize: 18,
+                                            color: AppColors.black38),
+                                    maxLines: null,
+                                    overflow: TextOverflow.visible,
+                                  ),
+                                  Center(
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        final url = AppConstants
+                                            .appServices[index]['url']!;
+                                        if (await canLaunchUrlString(url)) {
+                                          await launchUrlString(url);
+                                        } else {
+                                          throw 'Could not launch $url';
+                                        }
+                                      },
+                                      child: Text(
+                                        'Download',
+                                        style: AppTextStyles.poppinsBoldstyle18
+                                            .copyWith(color: AppColors.white),
+                                      ),
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: AppColors.primaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              onTap: () async {
+                final url = AppConstants.appServices[index]['url']!;
+                if (await canLaunchUrlString(url)) {
+                  await launchUrlString(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+            );
+          },
+        ),
+        floatingActionButton: SpeedDial(
+            spacing: 12,
+            spaceBetweenChildren: 12,
+            activeBackgroundColor: AppColors.primaryColor,
+            animatedIcon: AnimatedIcons.menu_close,
+            backgroundColor: AppColors.primaryColor,
+            overlayColor: AppColors.black,
+            animatedIconTheme: IconThemeData(color: AppColors.white),
+            overlayOpacity: 0.4,
+            // animatedIcon:AnimatedIcon.,
+            children: [
+              SpeedDialChild(
+                  onTap: () async {
+                    launchUrlString("https://wa.me/message/U7Z7VTYYG252C1");
+                  },
+                  child: Icon(
+                    FontAwesomeIcons.whatsapp,
+                    color: AppColors.white,
+                    size: 40,
+                  ),
+                  label: 'Whatsapp',
+                  labelStyle: AppTextStyles.poppinsBoldstyle16
+                      .copyWith(color: AppColors.black),
+                  backgroundColor: AppColors.green),
+              SpeedDialChild(
+                  onTap: () async {
+                    launchUrlString('https://t.me/a0abx');
+                  },
+                  child: Icon(
+                    Icons.telegram,
+                    size: 40,
+                    color: AppColors.white,
+                  ),
+                  label: 'Telegram',
+                  labelStyle: AppTextStyles.poppinsBoldstyle16
+                      .copyWith(color: AppColors.black),
+                  backgroundColor: AppColors.blue),
+              SpeedDialChild(
+                  onTap: () async {
+                    launchUrlString('tel:+963968782148');
+                  },
+                  child: Icon(
+                    Icons.call,
+                    color: AppColors.white,
+                    size: 40,
+                  ),
+                  label: 'Call',
+                  labelStyle: AppTextStyles.poppinsBoldstyle16
+                      .copyWith(color: AppColors.black),
+                  backgroundColor: AppColors.yellowGold),
+            ])
+        // Column(
+        //   mainAxisAlignment: MainAxisAlignment.end,
+        //   children: [
+        //     SizedBox(
+        //       height: 65,
+        //       width: 65,
+        //       child: FloatingActionButton(
+        //         onPressed: () async {
+        //           launchUrlString("https://wa.me/message/U7Z7VTYYG252C1");
+        //         },
+        //         backgroundColor: AppColors.green,
+        //         heroTag: 'whatsapp',
+        //         child: Icon(
+        //           FontAwesomeIcons.whatsapp,
+        //           color: AppColors.black,
+        //           size: 40,
+        //         ),
+        //       ),
+        //     ),
+        //     const SizedBox(height: 10),
+        //     SizedBox(
+        //       height: 65,
+        //       width: 65,
+        //       child: FloatingActionButton(
+        //         onPressed: () async {
+        //           launchUrlString('https://t.me/a0abx');
+        //         },
+        //         backgroundColor: AppColors.blue,
+        //         heroTag: 'telegram',
+        //         child: const Icon(
+        //           Icons.telegram,
+        //           size: 40,
+        //         ),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        );
+  }
+}
